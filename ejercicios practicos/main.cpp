927b671c8d05f5659bc74e923e02bd5eb483f892
Travi995 <joseantoniotravieso259@gmail.com>
2023-03-09 15:59:15 -0500

    JATB: num_primos

--- ejercicios practicos/main.cpp
+++ ejercicios practicos/main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
+/**
+Programa que determina si un número es primo o no utilizando un ciclo for
 
+Entrada: 7
+Salida: El número 7 es primo
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int entrada =0,valor =0;
+    cin>>entrada;
+
+        if( entrada!= 2 && entrada!=3 && entrada != 5){
+            if (entrada % 2 != 0 && entrada % 3 != 0 && entrada % 5 !=0){
+                cout<<" este es un numero primo"<<endl;
+            }else{
+                cout<<"este   nooooooooooooooo es un numero primo"<<endl;
+
+                }
+        }
+
+
+
 }
