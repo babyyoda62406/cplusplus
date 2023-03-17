c061af7413b301d547563f133af59b6e348b386a
Travi995 <joseantoniotravieso259@gmail.com>
2023-03-17 10:09:10 -0400

    JATB: actualizacion de carpetas

---
+++ Pilas/main.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main(){
+    stack <int> pila;
+    int a =0;
+     pila.push(20);
+     pila.push(15);
+
+     cout<<pila.top();
+
+
+
+
+    return 0;
+}
+
